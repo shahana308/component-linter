ff24d002c3ce17d7928f5ebacb3aed67c6840854
Shahana Salam <shahana.salam@cafu.com>
2025-06-24 22:16:06 +0400

    remove unused rule

--- dist/rules/index.js
+++ dist/rules/index.js
@@ -3,4 +3,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.rules = void 0;
 const namingRule_1 = require("./namingRule");
 const lengthRule_1 = require("./lengthRule");
-exports.rules = [namingRule_1.checkNamingRules, lengthRule_1.checkLengthRule, ,];
+exports.rules = [namingRule_1.checkNamingRules, lengthRule_1.checkLengthRule];
