f96dc28d4ea5dba1c6d1bb81bdcea32464b288dc
Shahana Salam <shahana.salam@cafu.com>
2025-06-25 21:11:43 +0400

    add hook cohesion rule and style rule

--- dist/rules/index.js
+++ dist/rules/index.js
@@ -3,4 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.rules = void 0;
 const namingRule_1 = require("./namingRule");
 const lengthRule_1 = require("./lengthRule");
-exports.rules = [namingRule_1.checkNamingRules, lengthRule_1.checkLengthRule];
+const hookCohesionRule_1 = require("./hookCohesionRule");
+const styleRule_1 = require("./styleRule");
+exports.rules = [
+    namingRule_1.checkNamingRules,
+    lengthRule_1.checkLengthRule,
+    hookCohesionRule_1.checkHookCohesionRule,
+    styleRule_1.checkInlineStyleRule,
+];
